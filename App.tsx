dbf0bd64893df50ca3bd3324648417f9fc99f66e
Дмитрий Иванов <monsgt-r@yandex.ru>
2019-12-09 13:37:00 +0700

    add Navbar

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Navbar} from "./src/Navbar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Navbar } from "./src/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Navbar/>
+      <Navbar title="Todo app" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {}
 });
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
-export const Navbar = (props) => {
-    return(
-        <View style={styles.navbar}>
-            <Text>Todo App</Text>
-        </View>
-    )
-}
+export const Navbar = ({ title }) => {
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    navbar: {
-        height: 70,
-        alignItems: 'center'
-    },
-    text: {}
-})
+  navbar: {
+    height: 70,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#3949ab",
+    paddingBottom: 10
+  },
+  text: {
+    color: "white",
+    fontSize: 20
+  }
+});
