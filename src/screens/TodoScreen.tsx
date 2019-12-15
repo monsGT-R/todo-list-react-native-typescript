24bd54cdf0720056f1f5b905b2ccf117f0285217
Дмитрий Иванов <monsgt-r@yandex.ru>
2019-12-15 18:00:45 +0700

    add style

--- src/screens/TodoScreen.tsx
+++ src/screens/TodoScreen.tsx
@@ -1,11 +1,45 @@
 import React from "react";
-import {View, Text, Button} from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { THEME } from "../theme";
+import { AppCard } from "../components/ui/AppCard";
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View>
-      <Text>{todo.title}</Text>
-      <Button title="Назад" onPress={goBack} />
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title="Ред." onPress={console.log} />
+      </AppCard>
+
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title="Назад" color={THEME.GRAY_COLOR} onPress={goBack} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Удалить"
+            color={THEME.DANGER_COLOR}
+            onPress={() => console.log("to Remove")}
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
+  button: {
+    width: "40%"
+  },
+  title: {
+    fontSize: 20
+  }
+});
---
+++ src/theme.tsx
@@ -0,0 +1,5 @@
+export const THEME = {
+  DANGER_COLOR: '#e33935',
+  GRAY_COLOR: '#757575',
+  MAIN_COLOR: '#3949ab'
+}
