3ace3556d1522c08580b4db7404ca55ee5fdd3dc
Дмитрий Иванов <monsgt-r@yandex.ru>
2019-12-20 00:07:41 +0700

    add empty list image

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,21 +1,45 @@
-import React, { useState } from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import React from "react";
+import { FlatList, Image, StyleSheet, View } from "react-native";
 import { AddTodo } from "../components/AddTodo";
 import { Todo } from "../components/Todo";
 
 export const MainScreen = ({ addTodo, todos, removeTodo, openTodo }) => {
+  let content = (
+    <FlatList
+      keyExtractor={(item: any) => item.id}
+      data={todos}
+      renderItem={({ item }) => (
+        <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
+      )}
+    />
+  );
+
+  if (todos.length === 0) {
+    content = (
+      <View style={styles.imgWrap}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/no-items.png")}
+        />
+      </View>
+    );
+  }
   return (
     <View>
       <AddTodo onSubmit={addTodo} />
-      <FlatList
-        keyExtractor={(item: any) => item.id}
-        data={todos}
-        renderItem={({ item }) => (
-          <Todo todo={item} onRemove={removeTodo} onOpen={openTodo} />
-        )}
-      />
+      {content}
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imgWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "85%"
+  },
+  image: {
+    width: "100%",
+    height: "100%"
+  }
+});
