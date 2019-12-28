a95989e3ac4fefec4e5219d8e7970115eca1d6c1
Дмитрий Иванов <monsgt-r@yandex.ru>
2019-12-29 01:22:05 +0700

    добавление в базу данных firebase

--- src/context/todo/TodoState.tsx
+++ src/context/todo/TodoState.tsx
@@ -1,18 +1,39 @@
 import React, { useContext, useReducer } from "react";
 import { TodoContext } from "./todoContex";
 import { todoReducer } from "./todoReducer";
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from "../types";
+import {
+  ADD_TODO,
+  CLEAR_ERROR,
+  HIDE_LOADER,
+  REMOVE_TODO,
+  SHOW_ERROR,
+  UPDATE_TODO
+} from "../types";
 import { ScreenContext } from "../screen/screenContext";
 import { Alert } from "react-native";
 
 export const TodoState = ({ children }) => {
   const initialState = {
-    todos: [{ id: "1", title: "Выучить React Native" }]
+    todos: [],
+    loading: false,
+    error: null
   };
   const { changeScreen } = useContext(ScreenContext);
   const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  const addTodo = title => dispatch({ type: ADD_TODO, title });
+  const addTodo = async title => {
+    const response = await fetch(
+      "https://rn-todo-app-10052.firebaseio.com/todos.json",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title })
+      }
+    );
+    const data = await response.json();
+    console.log('id', data.name);
+    dispatch({ type: ADD_TODO, title, id: data.name });
+  };
 
   const removeTodo = id => {
     const todo = state.todos.find(t => t.id === id);
@@ -38,6 +59,14 @@ export const TodoState = ({ children }) => {
   };
 
   const updateTodo = (id, title) => dispatch({ type: UPDATE_TODO, id, title });
+
+  const showLoader = () => dispatch({ type: SHOW_ERROR });
+
+  const hideLoader = () => dispatch({ type: HIDE_LOADER });
+
+  const showError = error => dispatch({ type: SHOW_ERROR, error });
+
+  const clearError = () => dispatch({ type: CLEAR_ERROR });
 
   return (
     <TodoContext.Provider
